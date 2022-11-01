c679f21c9eee9df7c67041d6d63800f42111f6c4
Leif Andrew Christian <mtmusicandart@gmail.com>
2022-11-01 08:58:07 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,66 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, TouchableOpacity, Dimensions } from 'react-native';
-import { WebView } from 'react-native-webview';
-import {useState, useEffect} from 'react'
-import { reloadAsync } from 'expo-updates';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { WebView } from "react-native-webview";
+import { useState, useEffect } from "react";
+import { reloadAsync } from "expo-updates";
 
 export default function App() {
+  const [theAppUrl, setTheAppUrl] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [howWide, setHowWide] = useState("");
+  const [howHigh, setHowHigh] = useState("");
 
- const [theAppUrl, setTheAppUrl] = useState('');
- const [toggle, setToggle] = useState(false)
- const [howWide, setHowWide] = useState('');
-const [howHigh, setHowHigh] = useState('');
-
-//  let width = Dimensions.get('window').width;
-//  let height = Dimensions.get('window').height;
-//  console.log(width, height)
+  //  let width = Dimensions.get('window').width;
+  //  let height = Dimensions.get('window').height;
+  //  console.log(width, height)
 
   const refresh = () => {
-    setToggle(prevState=>!prevState);
-    setTheAppUrl('http://www.google.com');
-  }
+    setToggle((prevState) => !prevState);
+    setTheAppUrl("http://www.google.com");
+  };
 
-useEffect(() => {
-  setTheAppUrl('http://54.215.36.230:3006')
-console.log(theAppUrl);
-}, [theAppUrl, toggle])
-
+  useEffect(() => {
+    setTheAppUrl("http://54.215.36.230:3006");
+    console.log(theAppUrl);
+  }, [theAppUrl, toggle]);
 
   return (
-    
-    <SafeAreaView style={{ paddingTop: '1%', backgroundColor: 'black', flex: 1 }}>
-
+    <SafeAreaView
+      style={{ paddingTop: "1%", backgroundColor: "black", flex: 1 }}
+    >
       <>
-      
-      <TouchableOpacity onPress={() => refresh()} title={'🔃'} style={{height: "12%", marginBottom: '1%', width: '100%'}}>
+        <TouchableOpacity
+          onPress={() => refresh()}
+          title={"🔃"}
+          style={{ height: "12%", marginBottom: "1%", width: "100%" }}
+        >
+          <Text
+            style={{
+              color: "red",
+              textAlign: "center",
+              marginTop: 40,
+              fontSize: 32,
+            }}
+          >
+            ♾️
+          </Text>
+        </TouchableOpacity>
 
- <Text style={{color:'red', textAlign: 'center', marginTop:40, fontSize:32}}>♾️</Text> 
-</TouchableOpacity> 
-      
-      <WebView style={{marginTop: '-4%'}}
-      source={{ uri: theAppUrl }} 
-    />
+        <WebView style={{ marginTop: "-4%" }} source={{ uri: theAppUrl }} />
 
-    {/* <Button ></Button> */}
-
-
-    
-
-    
+        {/* <Button ></Button> */}
       </>
-    
-  </SafeAreaView>
-
-  
-  
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
